2d1bea8ae4ab8ac0079ee7e555e7902c4ea727df
halison andrianaivoarivelo <halison.andria@gmail.com>
2025-10-26 23:29:40 +0100

    Fix - page/index.php

--- pages/index.php
+++ pages/index.php
@@ -1,3 +1,4 @@
+
 <section class="banner style1 orient-left content-align-left image-position-right fullscreen onload-image-fade-in onload-content-fade-right">
 	<div class="content">
 		<h1>Mon [ blog ].</h1>
@@ -12,24 +13,30 @@
 </section>
 
 <?php 
+	if (!function_exists('getArticlesFromJson')) {
+		header('Location: ../index.php');
+		exit();
+	}
+	
 	$_articles = getArticlesFromJson();
+	//var_dump($_articles);
 
 	if($_articles && count($_articles)){
 		$compteur = 1;
 		foreach($_articles as $article){
 			$classCss = ($compteur % 2 == 0 ? 'left' : 'right');
-			##$compteur++;
+			$compteur++;
 			?>
 				<section class="spotlight style1 orient-<?php echo $classCss;?>  content-align-left image-position-center onscroll-image-fade-in" id="first">
 					<div class="content">
 						<h2><?php echo $article['titre'];?></h2>
-						<p><?php echo $article['titre'];?></p>
+						<p><?php echo $article['texte'];?></p>
 						<ul class="actions stacked">
 							<li><a href="?page=article&id=<?php echo $article['id'];?>" class="button">Lire la suite</a></li>
 						</ul>
 					</div>
 					<div class="image">
-						<img src="<?php echo $art_icle['image'];?>" alt="" />
+						<img src="<?php echo $article['image'];?>" alt="" />
 					</div>
 				</section>
 
